91aa7ad50133f3fb2d630e5527cbc0f8e1b2d070
Pham_Dang_Duy <25021695@vnu.edu.vn>
2025-12-30 17:02:37 +0700

    test

--- Dstt_mini_project/Test.py
+++ Dstt_mini_project/Test.py
@@ -141,6 +141,133 @@ def delete_infor():
     else:
         print("Không tìm thấy!")
 
+def modify_infor(m=None):
+    import os
+    if m==None:
+        print("Nhập '<' hoặc '0' để quay lại.")
+        m=input('Nhập ID cần chỉnh sửa:')
+    if m=='<' or m=='0':
+        return False # ở đây return lại chương trình trước đó, có thể dùng os để xóa màn hình
+    while m not in temp_arr:
+        os.system('cls')
+        print("Nhập '<' hoặc '0' để quay lại.")
+        m=input('ID không tồn tại,vui lòng nhập lại:')
+        if m=='<' or m=='0':
+            return False # ở đây return lại chương trình trước đó, có thể dùng os để xóa màn hình
+    else:
+        os.system('cls')
+        Name_ban_đầu={temp_arr[m]["Họ và Tên"]}
+        Điểm_CC_ban_đầu={temp_arr[m]["Điểm CC"]}
+        Điểm_GK_ban_đầu= {temp_arr[m]["Điểm GK"]}
+        Điểm_CK_ban_đầu= {temp_arr[m]["Điểm CK"]}
+        Điểm_TB_ban_đầu={temp_arr[m]["Điểm TB"]}
+        Xác_nhận=False
+        while Xác_nhận==False:
+            ten=input(f"""
+Nhập '<' hoặc '_esc_' để quay lại.
+=======================================             
+                      
+          THÔNG TIN HỌC SINH                                     
+Họ và Tên: {Name_ban_đầu}                                
+Điểm CC : {Điểm_CC_ban_đầu}                         
+Điểm GK : {Điểm_GK_ban_đầu}                        
+Điểm CK : {Điểm_CK_ban_đầu}                         
+Điểm TB : {Điểm_TB_ban_đầu}                                          
+======================================= 
+=======================================
+            CHỈNH SỬA 
+Họ và Tên:
+Điểm CC:
+Điểm GK:
+Điểm CK:
+Điểm TB:
+=======================================
+Nhập tên:                                            
+""")
+            if ten== '<' or ten=='_esc_':
+                return False
+            os.system('cls')
+            điểm_cc=input(f"""      
+=======================================
+            CHỈNH SỬA
+Họ và Tên:{ten}
+Điểm CC:
+Điểm GK:
+Điểm CK:
+Điểm TB:
+=======================================
+Nhập điểm CC:                                                   
+""")
+            if điểm_cc== '<' or điểm_cc=='_esc_':
+                return False
+            os.system('cls')
+            điểm_gk=input(f""" 
+=======================================
+            CHỈNH SỬA 
+Họ và Tên:{ten}
+Điểm CC:{điểm_cc}
+Điểm GK:
+Điểm CK:
+Điểm TB:
+=======================================
+Nhập điểm GK:                                            
+""")
+            if điểm_gk== '<' or điểm_gk=='_esc_':
+                return False
+            os.system('cls')
+            điểm_ck=input(f""" 
+=======================================
+            CHỈNH SỬA 
+Họ và Tên:{ten}
+Điểm CC:{điểm_cc}
+Điểm GK:{điểm_gk}
+Điểm CK:
+Điểm TB:
+=======================================
+Nhập điểm CK:                                            
+""")
+            if điểm_ck== '<' or điểm_ck=='_esc_':
+                return False
+            os.system('cls')
+            xn=input(f""" 
+=======================================
+            CHỈNH SỬA 
+Họ và Tên:{ten}
+Điểm CC:{điểm_cc}
+Điểm GK:{điểm_gk}
+Điểm CK:{điểm_ck}
+Điểm TB:{float(điểm_cc)*0.1+float(điểm_gk)*0.3+float(điểm_ck)*0.6}
+=======================================
+Xác nhận thay đổi ? (y/n)                                
+""")
+            if xn=='y':
+                Xác_nhận=True
+                print('Đã chỉnh sửa thông tin')
+                temp_arr[m]={"Name": ten, "Điểm CC": điểm_cc, "Điểm GK": điểm_gk, "Điểm CK": điểm_ck, "Điểm TB": float(điểm_cc)*0.1+float(điểm_gk)*0.3+float(điểm_ck)*0.6}
+            if xn=='n':
+                xn_2=input("""
+==============================
+Chọn tác vụ:
+1.Sửa lại
+2.Quay vê
+""")
+                while xn_2!='1' and xn_2!='2':
+                    os.system('cls')
+                    print('Không hợp lệ')
+                    xn_2=input("""
+==============================
+Chọn tác vụ:
+1.Sửa lại
+2.Quay vê
+""")
+                    if xn_2=='2':
+                        return False
+                    if xn_2=='1':
+                        os.system('cls')
+
+
+
+
 def main():
     n = 0 
     while True:
@@ -154,6 +281,8 @@ def main():
         elif n == 3:
             Add_infor()
         elif n == 4:
+            modify_infor()
+        elif n == 5:
             return False
         n = 0
 main()
